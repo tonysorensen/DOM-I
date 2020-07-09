9f492e0ff310e261a13efa978c5ee7b69d3ca710
Tony Sorensen <tony48853@gmail.com>
2020-07-08 20:13:51 -0700

    All items on page but need to figure out how to insert line breaks in h1 and first contact paragraph and how to get text to render in the button

--- js/index.js
+++ js/index.js
@@ -69,12 +69,24 @@ topImg.src="img/header-img.png";
 
 
 //add text to 2 h4's and 2 p's in .top-content.text-content
-const topContentHeads = document.getElementsByTagName('h4');
+
+//This is total hackey but it works. I need to figure out how to access the children of the 'top-content' and 'bottom-content' divs by themselves.
+
+
+// const topContentHeads = document.getElementsByTagName('h4');
+// topContentHeads[0].textContent = 'Features'
+// topContentHeads[1].textContent = 'About';
+// const topContentPara = document.getElementsByTagName ('p');
+// topContentPara[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// topContentPara[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const topContentHeads = document.querySelectorAll('.top-content h4');
 topContentHeads[0].textContent = 'Features'
 topContentHeads[1].textContent = 'About';
-const topContentPara = document.getElementsByTagName ('p');
+const topContentPara = document.querySelectorAll('.top-content p');
 topContentPara[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-topContentPara[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+topContentPara[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
 
 //add source to .middle-img
 const midImg = document.getElementById ('middle-img')
@@ -82,12 +94,50 @@ midImg.src="img/mid-page-accent.jpg"
 
 
 
-//add text to 2 h4's and 2 p's in .bottom-content.text-content
+//add text to 2 h4's and 3 p's in .bottom-content.text-content
+
+//This is total hackey but it works. I need to figure out how to access the children of the 'top-content' and 'bottom-content' divs by themselves.
+
+// const bottomHeads = document.getElementsByTagName('h4')
+// console.log(bottomHeads)
+// bottomHeads[2].textContent = 'Services'
+// bottomHeads[3].textContent = 'Product'
+// bottomHeads[4].textContent = 'Vision'
+
+// const bottomContentPara = document.getElementsByTagName('p')
+// console.log(bottomContentPara)
+// bottomContentPara[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+// bottomContentPara[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+// bottomContentPara[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const bottomHeads = document.querySelectorAll('.bottom-content h4')
+console.log(bottomHeads)
+bottomHeads[0].textContent = 'Services'
+bottomHeads[1].textContent = 'Product'
+bottomHeads[2].textContent = 'Vision'
+
+const bottomContentPara = document.querySelectorAll('.bottom-content p')
+console.log(bottomContentPara)
+bottomContentPara[0].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+bottomContentPara[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+bottomContentPara[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
 
 //add text to h4 and p's in .contact
+const contact = document.querySelector('.contact h4')
+contact.textContent = 'Contact'
+const contactPara = document.querySelectorAll('.contact p')
+contactPara[0].textContent = '123 Way 456 Street Somewhere, USA'
+contactPara[1].textContent = '1 (888) 888-8888'
+contactPara[2].textContent = 'sales@greatidea.io'
 
 //add text to footer p
-
+const footerText = document.querySelector('footer p')
+footerText.textContent = 'Copyright Great Idea! 2018'
 
 
 
